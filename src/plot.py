e6d8ccd4ab6acdbb179e6b09659e851f9f6751ee
胡思源 <ktboyyy@outlook.com>
2018-04-11 11:29:30 -0400

    fix plot and imitation

--- src/plot.py
+++ src/plot.py
@@ -10,7 +10,7 @@ file = open("log.txt", "r")
 while True:
 	line = file.readline()
 	if line:
-		if line[0] == '-' or (line[0]>'0' and line[0]<'9'):
+		if line[0] == '-' or (line[0]>='0' and line[0]<='9'):
 			data = line.split()
 			reward.append(float(data[0]))
 			std.append(float(data[1]))
@@ -26,10 +26,10 @@ file.close()
 
 # print(reward)
 
-plt.plot(update, reward, c='b', label='reinforce')
+plt.plot(update, reward, c='b', label='a2c 20 step')
 
 plt.legend(loc='best')
-plt.ylabel('mean/std reward on '+str(step)+' episodes')
+plt.ylabel('mean/std reward on 100 episodes')
 plt.xlabel('episodes')
 plt.errorbar(update, reward, yerr=std, fmt='o')
 plt.grid()
